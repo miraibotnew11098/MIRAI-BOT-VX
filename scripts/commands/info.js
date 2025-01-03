d6a55fe727e8bfc37517e753118b61482831ea25
miraibotnew11098 <vff481945@gmail.com>
2025-01-03 16:19:40 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,19 +28,19 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       :  AKASH HASAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : AKASH HASAN
-𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : ISLAM
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: MYMENSINGH
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: MYMENSINGH
-𝐆𝐞𝐧𝐝𝐞𝐫.   : MALE
-𝐀𝐠𝐞           : 19+
-𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : SINGEL
-𝐖𝐨𝐫𝐤        : NOT FOUND
-𝐆𝐦𝐚𝐢𝐥       : akash768004@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801980871152
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/akash_black
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/akash.black.hacker.bd`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐍𝐚𝐦𝐞       :  𝗞𝗔𝗠𝗥𝗨𝗟 𝗛𝗔𝗦𝗔𝗡
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : ‎〲𝗧𝗢𝗠 卝 চৌধুরীヅ࿐
+𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝗜𝗦𝗟𝗔𝗠
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝗥𝗔𝗡𝗚𝗣𝗨𝗥
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝗗𝗛𝗔𝗞𝗔 𝗞𝗛𝗘𝗟 𝗞𝗛𝗘𝗧
+𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝗠𝗔𝗟𝗘
+𝐀𝐠𝐞           : 17+
+𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝗠𝗔𝗥𝗥𝗜𝗘𝗗
+𝐖𝐨𝐫𝐤        : 𝗟𝗨𝗖𝗖𝗛𝗔 𝗢𝗞𝗛
+𝐆𝐦𝐚𝐢𝐥       : 𝗘𝗪𝗥𝗣𝗜𝗫𝗫𝗜@𝗚𝗠𝗔𝗜𝗟.𝗖𝗢𝗠
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801328955648
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/𝗣𝗜𝗫𝗫𝗜𝗧𝗔𝗛
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/pixxi0`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://i.postimg.cc/gc86JvyL/1727951160044.jpg`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
